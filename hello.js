7945c88cbf86d2df33888a822b7c1aa9f06bb24f
Cxlopez <cristian4402@hotmail.com>
2022-01-05 14:14:27 -0500

    just saying whatsup

---
+++ hello.js
@@ -0,0 +1,15 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+ // new line
+
+ const sayHello = function (name) {
+   console.log ("hello, " + name);
+ };
+
+ sayHello("Cristian!");
+
+ 
+
